99a7ee18154b404012503bfe1e941c52d1fe038b
EliasEducation <18000053@edu.spbgasu.ru>
2021-09-24 16:20:04 +0300

    files for site

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>About me</title>
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+
+	<body>
+		<div class="wrapper">
+			<div class="top_nav">
+				<ul class="nav_menu">
+					<li>
+						<i class="name">Смертин Илья Витальевич</i>
+					</li>
+					<li class="delimiter"></li>
+					<li class="element">
+						<i class="element_content">Резюме</i>
+					</li>
+					<li class="element">
+						<i class="element_content">Портфолио</i>
+					</li>
+					<li class="element">
+						<i class="element_content">Проекты</i>
+					</li>
+					<li class="element">
+						<i class="element_content">Обо мне</i>
+					</li>
+					<li class="element">
+						<i class="element_content">Контакты</i>
+					</li>
+				</ul>
+				<hr>
+			</div>
+		</div>
+	</body>
+	<footer>123</footer>
+</html>
